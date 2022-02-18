1f6efc468d8b0c8107655e7c150ec3a1f829c704
daniebeler <hiebeler.daniel@gmail.com>
2022-02-18 19:32:17 +0100

    created project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,335 @@
+/* eslint-disable vue/multi-word-component-names */
+/* eslint-disable vue/multi-word-component-names */
+<template>
+  <nav class="navbar fixed-top navbar-holder">
+    <div id="myNav" class="overlay">
+      <div class="overlay-content">
+        <div>
+          <a class="effect-box" href="https://daniebeler.com">Home</a>
+        </div>
+        <a class="effect-box" href="https://daniebeler.com/about">About</a>
+        <a class="effect-box" href="https://daniebeler.com/contact">Contact</a>
+        <a class="effect-box" href="https://daniebeler.com/games">Games</a>
+        <a class="effect-box" href="https://daniebeler.com/webdesign">Webdesign</a>
+        <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+      </div>
+    </div>
+
+    <div class="headerz">
+      <div class="progress-container">
+        <div class="progressbar" id="myBar"></div>
+      </div>
+    </div>
+
+    <div class="container" style="z-index: 500">
+      <div class="row d-flex flex-grow-1 justify-content-between">
+        <div class="col"></div>
+
+        <div class="col d-flex justify-content-center">
+          <a href="https://daniebeler.com" class="navbar-brand icon-header"
+            >Daniel Hiebeler</a
+          >
+        </div>
+
+        <div class="col d-flex justify-content-end">
+          <button
+            style="z-index: 1030; outline: none"
+            onclick="toggleNav()"
+            class="nav-toggler"
+            type="button"
+          >
+            <div class="animated-icon1">
+              <span></span><span></span><span></span>
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <!-- <h1>meem</h1>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav> -->
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+.header {
+  position: fixed;
+  z-index: 10;
+  height: 50px;
+  background-color: black;
+}
+
+.not-draggable {
+  user-drag: none;
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+}
+
+.active-nav-item {
+  padding: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+  color: #ac7ffb;
+}
+
+.active-nav-item:hover {
+  text-decoration: none;
+  color: var(--my_dark);
+}
+
+.active-nav-item a {
+  color: #ac7ffb;
+}
+
+.active-nav-item a:hover {
+  text-decoration: none;
+  color: var(--my_dark);
+}
+
+.nav-item a {
+  color: var(--my_dark);
+}
+
+.nav-item a:hover {
+  text-decoration: none;
+  color: #ac7ffb;
+}
+
+.nav-item {
+  padding: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+  color: #ac7ffb;
+}
+
+.nav-item:hover {
+  text-decoration: none;
+  color: var(--my_dark);
+}
+
+.header-area.nav-item {
+  color: var(--my_dark);
+}
+
+.header-area.down.nav-item {
+  color: rgba(48, 107, 242, 1);
+}
+
+.header-area {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100px;
+  background: whitesmoke;
+  color: var(--my_dark);
+  z-index: 10;
+}
+
+.header-bar {
+  position: fixed;
+  height: 50px;
+}
+
+.header-size {
+  font-size: 3rem;
+  text-align: center;
+}
+
+@media (min-width: 768px) {
+  .header-size {
+    font-size: 4.5rem;
+    text-align: center;
+  }
+}
+
+.overlay {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 102;
+  top: 0;
+  left: 0;
+  background-color: #ff7128;
+  overflow-x: hidden;
+  transition: 0.5s;
+}
+
+.overlay-content {
+  position: relative;
+  top: 25%;
+  width: 100%;
+  text-align: center;
+  margin-top: 30px;
+}
+
+.overlay a {
+  padding: 8px;
+  text-decoration: none;
+  font-size: 36px;
+  color: #fff;
+  display: block;
+  transition: 0.3s;
+}
+
+.overlay a:hover,
+.overlay a:focus {
+  color: #f1f1f1;
+}
+
+.overlay .closebtn {
+  position: absolute;
+  top: 20px;
+  right: 45px;
+  font-size: 60px;
+}
+
+@media screen and (max-height: 450px) {
+  .overlay a {
+    font-size: 20px;
+  }
+  .overlay .closebtn {
+    font-size: 40px;
+    top: 15px;
+    right: 35px;
+  }
+}
+
+.animated-icon1,
+.animated-icon2,
+.animated-icon3 {
+  width: 30px;
+  height: 20px;
+  position: relative;
+  margin: 0px;
+  -webkit-transform: rotate(0deg);
+  -moz-transform: rotate(0deg);
+  -o-transform: rotate(0deg);
+  transform: rotate(0deg);
+  -webkit-transition: 0.5s ease-in-out;
+  -moz-transition: 0.5s ease-in-out;
+  -o-transition: 0.5s ease-in-out;
+  transition: 0.5s ease-in-out;
+  cursor: pointer;
+}
+
+.animated-icon1 span,
+.animated-icon2 span,
+.animated-icon3 span {
+  display: block;
+  position: absolute;
+  height: 4px;
+  width: 100%;
+  border-radius: 9px;
+  opacity: 1;
+  left: 0;
+  -webkit-transform: rotate(0deg);
+  -moz-transform: rotate(0deg);
+  -o-transform: rotate(0deg);
+  transform: rotate(0deg);
+  -webkit-transition: 0.25s ease-in-out;
+  -moz-transition: 0.25s ease-in-out;
+  -o-transition: 0.25s ease-in-out;
+  transition: 0.25s ease-in-out;
+}
+
+.animated-icon1 span {
+  background: #fff;
+}
+
+.animated-icon1 span:nth-child(1) {
+  top: 0px;
+}
+
+.animated-icon1 span:nth-child(2) {
+  top: 10px;
+}
+
+.animated-icon1 span:nth-child(3) {
+  top: 20px;
+}
+
+.animated-icon1.open span:nth-child(1) {
+  top: 11px;
+  -webkit-transform: rotate(135deg);
+  -moz-transform: rotate(135deg);
+  -o-transform: rotate(135deg);
+  transform: rotate(135deg);
+  background: #fff;
+}
+
+.animated-icon1.open span:nth-child(2) {
+  opacity: 0;
+  left: -60px;
+  background: #fff;
+}
+
+.animated-icon1.open span:nth-child(3) {
+  top: 11px;
+  -webkit-transform: rotate(-135deg);
+  -moz-transform: rotate(-135deg);
+  -o-transform: rotate(-135deg);
+  transform: rotate(-135deg);
+  background: #fff;
+}
+
+.navbar-wrapper {
+  z-index: 99;
+  position: fixed;
+  outline: none;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  transition: 1s;
+}
+
+.navbar-holder {
+  /* margin-top: 17px; */
+  margin-bottom: 15px;
+  transition: 1s;
+}
+
+.scrolled-navbar {
+  background-color: RGBA(255, 255, 255, 1);
+  transition: 0.5s;
+}
+
+.navbar-icon {
+  height: 75px;
+  z-index: 0;
+  border-radius: 50%;
+}
+
+.effect-box::before {
+  content: "</";
+  color: inherit;
+  opacity: 0;
+  transition: 0.5s;
+}
+
+.effect-box::after {
+  white-space: pre-wrap;
+  content: "> ";
+  opacity: 0;
+  transition: 0.5s;
+}
+
+.effect-box:hover::after,
+.effect-box:hover::before {
+  opacity: 1;
+  transition: 0.5s;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,466 @@
+<template>
+<div class="wrapper">
+
+  <section class=" page_1 d-flex align-items-center justify-content-center panel" data-color="grey">
+    <div class="container">
+      <div class="row justify-content-center">
+        <h1 class="heading mb-4">
+          I am Daniel Hiebeler
+        </h1>
+      </div>
+      <div class="row justify-content-center text-center">
+        <p class="hero-subtitle"><span class="typed"
+            data-typed-items="Sailor, Programmer, Game Developer, HTL Student"></span></p>
+      </div>
+    </div>
+
+    <div class="container fixed-bottom">
+      <div class="row">
+        <div class="col">
+          <div class="arrow bounce">
+            <img class="img-arrow-down not-draggable" src="@/assets/img/arrow-down.svg" alt="Arrow down">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="page_2 panel" data-color="grey">
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+        <div class="col-auto d-flex align-items-center">
+          <img class="m-4 img-logo not-draggable" src="@/assets/img/logo.webp" alt="Icon of ReflexTest">
+        </div>
+        <div class="d-flex col align-items-center">
+          <div>
+            <p>
+              Welcome to my page! Here you will find some facts about me and you can get a small overview
+              of my projects.<br><br>
+              <strong>A bit about me:</strong> At the moment I am attending fifth grade of HTL Dornbirn.
+              I love all kinds of programming, with passion in mobile game development. I also like online
+              challenges like TryHackMe or HackerRank. Especially OSINT challenges are a lot of fun...
+            </p>
+            <div class="row pt-3">
+              <div class="d-flex col justify-content-center">
+                <a href="about" class="btn_general btn_light">More Information</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="page_3 panel" data-color="white">
+
+    <div class="container">
+      <div class="row mt-5 mb-3" data-aos="zoom-in-up">
+        <h2 class="col">
+          Games
+        </h2>
+      </div>
+      <div class="row">
+
+        <div class="col-6 col-lg-3 col-md-4 pt-3 pb-3" data-aos="zoom-in-up">
+          <img class="img" src="@/assets/img/reflextest.webp" alt="Icon of ReflexTest">
+          <div class="m-3 img-overlay img-reflextest d-flex align-items-center justify-content-center"
+            onclick="location.href='games#reflextest';">
+            <div class="overlay-txt">Reflextest</div>
+          </div>
+        </div>
+
+        <div class="col-6 col-lg-3 pt-3 col-md-4 pb-3" data-aos="zoom-in-up">
+          <img class="img" src="@/assets/img/speed-clicker.webp" alt="Icon of Speed Clicker">
+          <div class="m-3 img-overlay img-speedclicker d-flex align-items-center justify-content-center"
+            onclick="location.href='games#speedclicker';">
+            <div class="overlay-txt">Speed Clicker</div>
+          </div>
+        </div>
+
+        <div class="col-6 col-lg-3 pt-3 col-md-4 pb-3" data-aos="zoom-in-up">
+          <img class="img" src="@/assets/img/21.webp" alt="Icon of Twentyone">
+          <div class="m-3 img-overlay img-twentyone d-flex align-items-center justify-content-center"
+            onclick="location.href='games#twentyone';">
+            <div class="overlay-txt">Twentyone</div>
+          </div>
+        </div>
+
+        <div class="col-6 col-lg-3 pt-3 col-md-4 pb-3" data-aos="zoom-in-up">
+          <img class="img" src="@/assets/img/mine-boy.webp" alt="Icon of MineBoy">
+          <div class="m-3 img-overlay img-mineboy d-flex align-items-center justify-content-center"
+            onclick="location.href='games#mineboy';">
+            <div class="overlay-txt">MineBoy</div>
+          </div>
+        </div>
+
+        <div class="col-6 col-lg-3 pt-3 col-md-4 pb-3" data-aos="zoom-in-up">
+          <img class="img" src="@/assets/img/toggle-together.webp" alt="Icon of Toggle Together">
+          <div class="m-3 img-overlay img-toggletogether d-flex align-items-center justify-content-center"
+            onclick="location.href='games#toggletogether';">
+            <div class="overlay-txt">Toggle Together</div>
+          </div>
+        </div>
+
+        <div class="col-6 col-lg-3 pt-3 col-md-4 pb-3" data-aos="zoom-in-up">
+          <img class="img" src="@/assets/img/justwatchad.webp" alt="Icon of Just Watch Ad">
+          <div class="m-3 img-overlay img-justwatchad d-flex align-items-center justify-content-center"
+            onclick="location.href='games#justwatchad';">
+            <div class="overlay-txt">Just Watch Ad</div>
+          </div>
+        </div>
+
+        <div class="col-6 col-lg-3 pt-3 col-md-4 pb-3" data-aos="zoom-in-up">
+          <img class="img" src="@/assets/img/parkingjam.webp" alt="Icon of Parking Jam">
+          <div class="m-3 img-overlay img-parkingjam d-flex align-items-center justify-content-center"
+            onclick="location.href='games#parkingjam';">
+            <div class="overlay-txt">Parking Jam</div>
+          </div>
+        </div>
+
+        <div class="col-6 col-lg-3 pt-3 col-md-4 pb-3" data-aos="zoom-in-up">
+          <img class="img" src="@/assets/img/wrongway.webp" alt="Icon of Wrong Way">
+          <div class="m-3 img-overlay img-wrongway d-flex align-items-center justify-content-center"
+            onclick="location.href='games#wrongway';">
+            <div class="overlay-txt">Wrong Way</div>
+          </div>
+        </div>
+
+        <div class="col-6 col-lg-3 pt-3 col-md-4 pb-3" data-aos="zoom-in-up">
+          <img class="img" src="@/assets/img/balance.webp" alt="Icon of Balance">
+          <div class="m-3 img-overlay img-balance d-flex align-items-center justify-content-center"
+            onclick="location.href='games#balance';">
+            <div class="overlay-txt">Balance - The Game</div>
+          </div>
+        </div>
+
+        <div class="col-6 col-lg-3 pt-3 col-md-4 pb-3" data-aos="zoom-in-up">
+          <img class="img" src="@/assets/img/pong.webp" alt="Icon of Daily Tasks">
+          <div class="m-3 img-overlay img-pong d-flex align-items-center justify-content-center"
+            onclick="location.href='games#pong';">
+            <div class="overlay-txt">1D Pong</div>
+          </div>
+        </div>
+
+        <div class="col-6 col-lg-3 pt-3 col-md-4 pb-3" data-aos="zoom-in-up">
+          <img class="img" src="@/assets/img/rocket.webp" alt="Icon of Daily Tasks">
+          <div class="m-3 img-overlay img-rocket d-flex align-items-center justify-content-center"
+            onclick="location.href='games#riskyrocket';">
+            <div class="overlay-txt">Risky Rocket</div>
+          </div>
+        </div>
+
+      </div>
+      <div class="row mt-5 mb-3" data-aos="zoom-in-up">
+        <h2 class="col">
+          Other projects
+        </h2>
+      </div>
+      <div class="row">
+        <div class="col-6 col-lg-3 pt-3 col-md-4 pb-3" data-aos="zoom-in-up">
+          <img class="img" src="@/assets/img/dailytasks.webp" alt="Icon of Daily Tasks">
+          <div class="m-3 img-overlay img-dailytasks d-flex align-items-center justify-content-center"
+            onclick="location.href='https://play.google.com/store/apps/details?id=com.daniebeler.dailytasks';">
+            <div class="overlay-txt">Daily Tasks</div>
+          </div>
+        </div>
+
+        <div class="col-6 col-lg-3 pt-3 col-md-4 pb-3" data-aos="zoom-in-up">
+          <img class="img" src="@/assets/img/idea-overflow.svg" alt="Icon of Daily Tasks">
+          <div class="m-3 img-overlay img-ideaoverflow d-flex align-items-center justify-content-center"
+            onclick="location.href='https://ideaoverflow.xyz';">
+            <div class="overlay-txt">Idea Overflow</div>
+          </div>
+        </div>
+
+        <div class="col-6 col-lg-3 pt-3 col-md-4 pb-3" data-aos="zoom-in-up">
+          <img class="img" src="@/assets/img/dine.png" alt="Icon of Daily Tasks">
+          <div class="m-3 img-overlay img-dine d-flex align-items-center justify-content-center"
+            onclick="location.href='https://dine-app.at';">
+            <div class="overlay-txt">dine.</div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="row" data-aos="zoom-in-up" style="margin-top: 20px;">
+        <div class="d-flex col justify-content-center">
+          <a href="games" class="btn_general btn_dark">More Information</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+  }
+})
+</script>
+
+<style scoped>
+.wrapper {
+  transition: background-color 1s ease;
+  background-color: #23272D;
+}
+
+.page_1 {
+  height: 100vh;
+  color: var(--my_dark);
+  flex-direction: column;
+}
+
+.page_2 {
+  color: var(--my_light);
+  font-size: 1.2rem;
+  padding-top: 200px;
+  padding-bottom: 100px;
+}
+
+.page_3 {
+  padding-top: 150px;
+  padding-bottom: 50px;
+  color: var(--my_dark);
+  position: relative;
+}
+
+.projects-heading {
+  color: #eb3349;
+}
+
+.heading {
+  color: whitesmoke;
+  font-size: 3rem;
+  text-align: center;
+}
+
+@media (min-width: 768px) {
+  .heading {
+    color: whitesmoke;
+    font-size: 4.5rem;
+    text-align: center;
+  }
+}
+
+.project-holder {
+  background-color: #f3f5f6;
+  padding: 0.9rem 1.1rem 0.9rem;
+  border-radius: 10px;
+  margin-bottom: 3rem;
+  transition: 0.5s;
+}
+
+.project-holder:hover {
+  cursor: pointer;
+  transform: translateY(-10px);
+}
+
+.about-holder {
+  padding-left: 30px;
+  padding-right: 30px;
+}
+
+.img-container {
+  width: 100%;
+  padding: 15px;
+}
+
+.img-border {
+  width: 100%;
+  border-radius: 10px;
+}
+
+@keyframes change {
+  0% {
+    background-position: 0 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0 50%;
+  }
+}
+
+/*----------------------------------------*/
+/*  Arrow Down
+/*----------------------------------------*/
+
+.img-arrow-down {
+  width: 50px;
+}
+
+.arrow {
+  text-align: center;
+  bottom: 20%;
+}
+
+.bounce {
+  -moz-animation: bounce 2s infinite;
+  -webkit-animation: bounce 2s infinite;
+  animation: bounce 2s infinite;
+}
+
+@keyframes bounce {
+  0%,
+  20%,
+  50%,
+  80%,
+  100% {
+    transform: translateY(0);
+  }
+  40% {
+    transform: translateY(-30px);
+  }
+  60% {
+    transform: translateY(-15px);
+  }
+}
+
+.fixed-bottom {
+  position: absolute;
+  bottom: 20%;
+  z-index: 100;
+}
+
+.table-section-2 {
+  color: var(--my_light);
+}
+
+.img {
+  width: 100%;
+  border-radius: 15px;
+  box-shadow: 4px 12px 40px 6px rgba(0, 0, 0, 0.09);
+}
+
+.img:hover {
+  cursor: pointer;
+}
+
+.img-overlay {
+  position: absolute;
+  border-radius: 15px;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  opacity: 0;
+  transition: 0.5s ease;
+}
+
+.img-reflextest {
+  background-color: #0dbc3e;
+}
+
+.img-speedclicker {
+  background-color: #2ca6e9;
+}
+
+.img-twentyone {
+  background-color: #5e35b1;
+}
+
+.img-mineboy {
+  background-color: #653814;
+}
+
+.img-toggletogether {
+  background-color: #2279ff;
+}
+
+.img-justwatchad {
+  background-color: #ffb602;
+}
+
+.img-parkingjam {
+  background-color: #ed8923;
+}
+
+.img-wrongway {
+  background-color: #32aa50;
+}
+
+.img-balance {
+  background-color: #ffc656;
+}
+
+.img-pong {
+  background-color: #ef6060;
+}
+
+.img-rocket {
+  background-color: #31c381;
+}
+
+.img-dailytasks {
+  background-color: #212121;
+}
+
+.img-ideaoverflow {
+  background-color: #0cc87e;
+}
+
+.img-dine {
+  background-color: #dfcfb0;
+}
+
+/* .img-overlay:hover {
+  opacity: 1;
+  cursor: pointer;
+} */
+
+.overlay-txt {
+  color: white;
+}
+
+.bg-1 {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  z-index: 1;
+  background-color: var(--my_dark);
+}
+
+.bg-2 {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  z-index: 2;
+  background-color: var(--my_dark);
+  transition: background-color 0.5s ease;
+}
+
+.color-grey {
+  background-color: var(--my_dark);
+}
+
+.color-red {
+  background-color: #dc3545;
+}
+
+.color-white {
+  background-color: var(--my_light);
+}
+
+.color-transparent {
+  background-color: transparent;
+}
+
+.img-logo {
+  max-width: 250px;
+}
+
+.hero-subtitle{
+  color: #dc3545;
+  font-size: 1.5rem;
+}
+
+@media (min-width: 768px) {
+  .hero-subtitle {
+    color: #dc3545;
+    font-size: 2.5rem;
+  }
+}
+</style>
