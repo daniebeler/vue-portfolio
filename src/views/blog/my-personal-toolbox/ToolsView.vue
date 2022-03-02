ffc1ffd3a02e1c706b449d6d56c638dd5016c166
daniebeler <hiebeler.daniel@gmail.com>
2022-03-02 19:53:33 +0100

    worked on blog post

--- src/views/blog/my-personal-toolbox/ToolsView.vue
+++ src/views/blog/my-personal-toolbox/ToolsView.vue
@@ -19,10 +19,39 @@
           <p><strong>WhatsApp</strong> - communication</p>
           <p><strong>Discord</strong> - voicechat</p>
 
-          <h1>Tools</h1>
+          <h1>Text Editors</h1>
+          <p><strong>Visual Studio Code</strong> - programming</p>
+          <p><strong>Libre Office</strong> - general</p>
+          <p><strong>Microsoft Office</strong> - general</p>
+
+          <h1>Web Browser</h1>
+          <p><strong>Mozilla Firefox</strong> - main browser</p>
+          <p><strong>Google Chrome</strong> - Webdesign testing</p>
+
+          <h1>Browser Extensions</h1>
+          <p><strong>Wappalyzer</strong> - technology profiler for websites</p>
+          <p><strong>Font Ninja</strong> - Font finder for websites</p>
+          <p><strong>Honey</strong> - coupons finder</p>
+          <p><strong>AdBlocker Ultimate</strong> - adblocker</p>
+
+          <h1>Operating System</h1>
+          <p><strong>Microsoft Windows</strong> - dual boot</p>
+          <p><strong>Linux Mint</strong> - dual boot</p>
+
+          <h1>Others</h1>
+          <p><strong>Flameshot</strong> - Screenshots</p>
+          <p><strong>Windscribe</strong> - VPN</p>
+          <p><strong>VirtualBox</strong> - Virtualization</p>
+          <p><strong>Termius</strong> - SSH Client</p>
+          <p><strong>DBeaver</strong> - Database Client</p>
+
+          <h1>VS Code extensions</h1>
+          <p><strong>CodeSnap</strong> - Screenshots</p>
+          <p><strong>Material Theme</strong> - Theme</p>
+          <p><strong>Material Icon Theme</strong> - Icon Theme</p>
+          <p><strong>Prettier</strong> - code formatter</p>
         </div>
       </div>
-      fief
     </div>
   </section>
 </template>
