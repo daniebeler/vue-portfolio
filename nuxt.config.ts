59f46dbf1d70a17b0827c58078c3c308d7081ad8
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-05-01 21:18:30 +0200

    Add metadata and fix formatting of privacy policy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,33 +2,54 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  vite: { plugins: [tailwindcss(),], },
-  modules: ['@nuxt/fonts', 'nuxt-schema-org'],
+  css: ["~/assets/css/main.css"],
+  vite: { plugins: [tailwindcss()] },
+  modules: ["@nuxt/fonts", "nuxt-schema-org"],
   site: {
-    host: 'https://daniebeler.com',
+    host: "https://daniebeler.com",
   },
   app: {
     head: {
       link: [
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }
-      ]
-    }
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "canonical",
+          href: "https://daniebeler.com",
+        },
+      ],
+      htmlAttrs: {
+        lang: "en",
+      },
+    },
   },
   schemaOrg: {
-    defaults: false
+    defaults: false,
   },
   fonts: {
     experimental: {
       processCSSVariables: true,
     },
     families: [
-      { name: 'Poppins', provider: 'google', weight: 400 },
-      { name: 'Poppins', provider: 'google', weight: 700 },
-    ]
-  }
-})
+      { name: "Poppins", provider: "google", weight: 400 },
+      { name: "Poppins", provider: "google", weight: 700 },
+    ],
+  },
+});
